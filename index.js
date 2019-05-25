390cc6e4e99e5218290e4a56421adcd6232a3d2c
vfxholic <vfxhollic@gmail.com>
2019-05-25 14:30:36 +0900

    array, object

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const daysOfWeek = ["Mon", "Thu", "Wed"];
+const nicoInfo = {
+  name: "nicolas",
+  age: 27,
+  gender: "male",
+  isHandsome: true,
+  favmovie: ["along the gods", "losiha", "oldboy"],
+  favfood: [
+    { name: "kimchi", fatty: "false" },
+    { name: "cheese burger", fatty: "ture" }
+  ]
+};
 
-console.log(daysOfWeek[2]);
+nicoInfo.gender = "Female";
+
+console.log(nicoInfo.gender);
+console.log(nicoInfo.favmovie);
+console.log(nicoInfo.favfood[0].name);
