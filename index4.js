7bd73256d8f7443c8940abf2f4d50ccbbb0ffedc
vfxholic <vfxhollic@gmail.com>
2019-05-25 17:26:11 +0900

    function update

--- index4.js
+++ index4.js
@@ -19,9 +19,6 @@ if (20 > 5 && "nicolas" === "nicolas") {
 
 /* 섹시하지 않아서 사용하지 않음 ㅋㅋㅋ 
 const age = prompt("how old are you?");
-console.log(age); */
-
-const age = prompt("how old are you?");
 
 if (age >= 18 && age <= 21) {
   console.log("you can dirnk but you should not");
@@ -29,4 +26,41 @@ if (age >= 18 && age <= 21) {
   console.log("go ahead");
 } else {
   console.log("too young");
+} */
+
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
+
+function handleOffline() {
+  console.log("bye bye~~!");
+}
+
+function handleOnline() {
+  console.log("Welcome back!!");
+}
+
+/*------------------------------------------*/
+
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+
+function OnOffline() {
+  window.addEventListener("offline", handleOffline);
+  window.addEventListener("online", handleOnline);
+}
+
+init();
+OnOffline();
