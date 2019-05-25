ee251b95a5d561401b09881b00dc0f4f1641e4ee
vfxholic <vfxhollic@gmail.com>
2019-05-25 18:11:11 +0900

    complite

--- index4.js
+++ index4.js
@@ -30,16 +30,18 @@ if (age >= 18 && age <= 21) {
 
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
+  title.classList.toggle(CLICKED_CLASS);
+  /*
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if (hasClass) {
+    title.classList.remove(CLICKED_CLASS);
   } else {
-    title.style.color = BASE_COLOR;
+    title.classList.add(CLICKED_CLASS);
   }
+  */
 }
 
 function handleOffline() {
@@ -53,7 +55,6 @@ function handleOnline() {
 /*------------------------------------------*/
 
 function init() {
-  title.style.color = BASE_COLOR;
   title.addEventListener("click", handleClick);
 }
 
