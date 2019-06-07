f45fac245b60f5d80e78441f90b4d4508a33e55c
vfxholic <vfxhollic@gmail.com>
2019-06-07 19:37:54 +0900

    javascript clock one

--- index4.js
+++ index4.js
@@ -33,15 +33,12 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  title.classList.toggle(CLICKED_CLASS);
-  /*
   const hasClass = title.classList.contains(CLICKED_CLASS);
   if (hasClass) {
     title.classList.remove(CLICKED_CLASS);
   } else {
     title.classList.add(CLICKED_CLASS);
   }
-  */
 }
 
 function handleOffline() {
